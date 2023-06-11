d4cca8718dadd955026d0847a55bace96b968e48
Woogeun Tak <dnrms47@naver.com>
2023-06-11 14:02:01 +0900

    4강 웹 서버와 웹 문서

---
+++ FrontEnd_Coments/4강 웹서버와 웹문서.cs
@@ -0,0 +1,7 @@
+[ Web과 HTTP ]
+
+웹 서버 : 문서를 제공하는 쪽 ( iis, aphach 등등, www.naver.com )
+웹 홈디렉토리 : 웹 서버 안에 있는 여러 디렉토리 ( url 페이지 GET요청 ) 
+=> ex http://www.naver.com/Webtoon)의 Webtoon 
+웹 클라이언트 : 웹 브라이우저 
+
