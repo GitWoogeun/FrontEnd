45fa33d775d8e5d495fefe02f7a8b4046c434833
Woogeun Tak <dnrms47@naver.com>
2023-06-11 14:59:34 +0900

    7강 HyperText

---
+++ FrontEnd_Coments/7강 HyperText.cs
@@ -0,0 +1,17 @@
+[ HyperText ]
+
+HTML : Hyper Text Markup Language
+
+[ Hyper Text Markup Language ]
+안녕하세요~ <b>개발자</b>입니다. <font color="blue">개발자 강의는</font> 
+단편적인 지식보다는 <u>용어의 올바른 이해</u>와 그것을 
+제대로 사용할 수 있도록 하는 <i>올바른 코딩 지침<i>을 알려주고 있습니다.
+
+
+안녕하세요~ <b><a href="http://www.google.co.kr">개발자</a></b>입니다. <font color="blue">개발자 강의는</font> 
+단편적인 지식보다는 <u>용어의 올바른 이해</u>와 그것을 
+제대로 사용할 수 있도록 하는 <i>올바른 코딩 지침<i>을 알려주고 있습니다.
+
+<a> 태그 : HyperLink 사용 ( 다른 페이지 열기 )
+
+
