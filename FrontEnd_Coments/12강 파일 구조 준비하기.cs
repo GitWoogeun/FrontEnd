e2d314b7c525e008ed444b56308b475d37aeccbf
Woogeun Tak <dnrms47@naver.com>
2023-06-18 22:21:41 +0900

    12강 실습을위한 파일구조

---
+++ FrontEnd_Coments/12강 파일 구조 준비하기.cs
@@ -0,0 +1,21 @@
+[ 12강 파일구조 준비하기 ]
+
+앞으로 만들게 될 HTML 문서들
+
+문서들의 연결관계
+
+실습 예제의 물리 구조잡기
+
+폴더 잡기 
+< 보다 바람직한 구조 >
+	root
+		index.html
+		[ notice_folder ]
+					=> list.html
+					=> dtail.html
+		[ member ]
+					=> login.html
+					=> signup-agree.html
+					=> signup.html
+
+폴더 생성
